7789bcef7d233ab4e544e506f20847cc1d48dc71
Divya Gambhir <114723756+Diald@users.noreply.github.com>
2024-01-02 11:26:49 +0530

    Medium

---
+++ Medium/Smallest Integer Divisible by K - Medium.py
@@ -0,0 +1,37 @@
+'''Given a positive integer k, you need to find the length of the smallest positive integer n such that n is divisible by k, and n only contains the digit 1.
+
+Return the length of n. If there is no such n, return -1.
+
+Note: n may not fit in a 64-bit signed integer.
+
+ 
+
+Example 1:
+
+Input: k = 1
+Output: 1
+Explanation: The smallest answer is n = 1, which has length 1.
+Example 2:
+
+Input: k = 2
+Output: -1
+Explanation: There is no such positive integer n divisible by 2.
+Example 3:
+
+Input: k = 3
+Output: 3
+Explanation: The smallest answer is n = 111, which has length 3.
+ 
+
+Constraints:
+
+1 <= k <= 105'''
+
+if(k%2 == 0 or k%5==0):
+  return -1
+prev_num = 0
+for n in range(k):
+  prev_num = ((prev_num*10)+1)%k
+  if(prev_num == 0):
+    return n+1
+return -1
